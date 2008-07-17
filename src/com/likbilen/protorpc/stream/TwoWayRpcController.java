b2eaadd7c377507eb6d702298d04e3d39571fe2b
Frederik M.J. Vestre <freqmod@iskrembilen.com>
2008-07-17 23:06:35 +0200

    forgot file

---
+++ src/com/likbilen/protorpc/stream/TwoWayRpcController.java
@@ -0,0 +1,37 @@
+package com.likbilen.protorpc.stream;
+
+/* Copyright (C) 2008 Frederik M.J. Vestre
+
+*   Licensed under the Apache License, Version 2.0 (the "License");
+*   you may not use this file except in compliance with the License.
+*   You may obtain a copy of the License at
+
+*   http://www.apache.org/licenses/LICENSE-2.0
+*/
+
+import com.google.protobuf.RpcChannel;
+import com.likbilen.protorpc.client.SimpleRpcController;
+import com.likbilen.protorpc.stream.session.SessionRpcController;
+import com.likbilen.protorpc.stream.session.TwoWayRpcChannelController;
+
+class TwoWayRpcController extends SimpleRpcController implements SessionRpcController,TwoWayRpcChannelController {
+	private TwoWayStream str;
+	public TwoWayRpcController(TwoWayStream str) {
+		super();
+		this.str=str;
+	}
+	@Override
+	public Object getSessionId() {
+		return str.getSessionId();
+	}
+	@Override
+	public void setSessionId(Object id) {
+		str.setSessionId(id);
+	}
+	public  TwoWayStream getTwoWayStream(){
+		return str;
+	}
+	public RpcChannel getRpcChannel(){
+		return str;
+	}
+}
